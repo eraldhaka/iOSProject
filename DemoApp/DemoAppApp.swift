bbf27354e2b4699179c15dc0bdaca56a6d4cdcbc
Daniel <daniel@livecareusa.com>
2022-06-30 15:17:19 -0400

    adding title

--- DemoApp/DemoAppApp.swift
+++ DemoApp/DemoAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct DemoAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
         }
     }
 }
